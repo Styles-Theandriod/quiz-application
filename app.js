84bf3b113e0a40aeebc735ef3a7c4d1129483ac0
Styles-Theandriod <emmanuelajah188@gmail.com>
2023-12-08 11:34:56 +0100

    not my first git

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+
+let currentQuestion = 0;
+let score = 0
+let scoreElement = document.getElementById("score")
+let questionElement = document.getElementById('question')
+let optionElement = document.getElementById('options')
+
+function loadQuestion(){
+    const currentQuizData = quizData[currentQuestion]
+    questionElement.textContent = currentQuizData.question
+
+    optionElement.innerHTML = ' '
+    currentQuizData.Options.forEach((option, index)=>{
+        const li = document.createElement('li')
+        li.textContent = option
+        li.classList.add("option")
+        li.onclick = () =>  selectOption(option)
+        optionElement.appendChild(li)
+    })
+}
+
+function selectOption(selectedOption){
+    quizData[currentQuestion].selected = selectedOption
+}
+
+
+function checkAnswer(){
+    const currentQuizData = quizData[currentQuestion]
+
+    if(currentQuizData.selected === currentQuizData.correctAnswer){
+        score++
+    }
+
+    currentQuestion++
+
+    if(currentQuestion < quizData.length){
+        loadQuestion()
+    }else{
+        finishQuiz()
+    }
+
+    function finishQuiz(){
+
+        if(currentQuestion == quizData.length){
+
+            scoreElement.textContent = `Your final score is: ${score} out of ${quizData.length} questions` 
+        }
+        // scoreElement.textContent = `Your final score is: ${score} out of ${quizData.length} questions` 
+        // scoreElement.textContent = 'Your final score is:' + score + 'out of' + quizData.length + 'questions' 
+    }
+    finishQuiz()
+}
+
+
+loadQuestion()
+
+
+/* loops that i know are 
+for loop
+for in loop 
+for of loop
+while loop
+do while loop */
+
+
+
+
+
